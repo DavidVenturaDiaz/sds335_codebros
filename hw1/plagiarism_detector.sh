7ab6755ce96a0630903278d1935ecda4477595aa
Abdon02 <averdejo02@utexas.edu>
2022-09-01 14:09:32 -0500

    Made a few changes to the edges cases

--- hw1/plagiarism_detector.sh
+++ hw1/plagiarism_detector.sh
@@ -1,9 +1,9 @@
 #!//usr/bin/bash
 
 #Make sure that the user inputted only 2 arguments
-if [ ![ [ $# -ge 1 ] && [ $# -le 2 ] ] ] ; then
-    echo "This is invalid number of arguments"
-    echo "This program should be ran like this: "
+if [ $# -lt 2 ] || [ $# -gt 2 ] ; then
+    echo "Invalid number of arguments."
+    echo "This is the correct way to run this script: "
     echo "./plagiarism_detector.sh <file> <directory>"
     exit 1
 fi
@@ -16,5 +16,15 @@ elif [ ! -d $2 ] ; then
     echo "Please input a directory for the second argument"
     exit 1
 fi
+
+#This will get all the items that are inside the directory
+#ls ./$2 | grep $1
+
+#if the file is different from anything in the directory, it should be copied into the directory; the script should not produce any output
+
+
+#if the file is the same as a file in the directory, the script should complain
+
+#test whether files are 'the same' should be made with the diff command. 
 
 
