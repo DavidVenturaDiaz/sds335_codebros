1fe7fcdc11cdb71f80510cd3a390e80f5fb4946d
Abdon02 <averdejo02@utexas.edu>
2022-09-04 15:35:48 -0500

    It is failing the second test case and I dont know why. It's annoying

--- hw1/plagiarism_detector.sh
+++ hw1/plagiarism_detector.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+##################################################
+# David Ventura Dias, 
+# Abdon Verdejo-Parada, av34626
+# hw1 sds335
+##################################################
 
 #Make sure that the user inputted only 2 arguments
 if [ $# -lt 2 ] || [ $# -gt 2 ] ; then
@@ -26,19 +32,24 @@ number_of_files="$(ls -la $2 | grep -c "^-rw.")"
 
 #If the directory had no files, then it gets added automatically
 if [ ! $number_of_files -eq 0 ] ; then
+
     #We now run through all the files in that directory
     for file in "$2"* ; do
-	#This is the number of lines that are displayed if they are different
-	result="$( diff $1 $file | wc -l )"
+	
+	#This will get the number of lines outputted from diff
+	#if the value of result is 0, then the files are the same
+	# if they are not the same, then the files are different
+	result="$( diff $1 $file | wc -l )" 
 	
 	#If the variable result is 0, then the files are identical
 	if [ $result -eq 0 ] ; then
-	    echo "The files $1 and $file are the same!!"
+	    echo "The files $1 and $file are identical!!"
 	    exit 1
 	fi
-	
+    
     done
 fi
 
 #If there was no complaints then the inputted file gets coppied into the inputted directory
 cp $1 ./$2
+#echo "The file $1 was copied into $2 directory"
