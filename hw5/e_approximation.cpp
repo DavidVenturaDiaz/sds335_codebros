5c4d030c572b98a42f2f70a9e93b4d5e7de3e01c
Abdon02 <averdejo02@utexas.edu>
2022-10-17 16:16:02 -0500

    Added the taylor series expansion

--- hw5/e_approximation.cpp
+++ hw5/e_approximation.cpp
@@ -8,7 +8,7 @@ void limitApproximation(){
   double e_approx;
   
   //Starting remark to the user
-  cout << "This is approximation of e using the limit method\n";
+  cout << "\nThis is approximation of e using the limit method\n\n";
 
   for(int i = 1; i <= 10; i++){
     n = pow(10, i);
@@ -22,9 +22,55 @@ void limitApproximation(){
   return;
 }
 
+double factorial(double val){
+  if(val == 0 || val == 1){
+    return 1;
+  }
+  
+  return val * factorial(val - 1);
+}
+
+void taylorExpansion(int &uptoVal){
+  //This is the starting e approximation
+  double e_approximation = 0;
+
+  //This will do the taylor series expansion
+  for(int i = 0; i < uptoVal; i++){
+    e_approximation = e_approximation + (1 / factorial(i));
+  }
+
+  //Result of this function
+  cout << "The approximation of e when N is: " << uptoVal << " is: " << e_approximation << "\n";
+
+  //End of function
+  return;
+}
+
+void automateTaylorSeries(int &valIterations){
+  //This will tell the user what function is being used to approximate e
+  cout << "\nThis is an approximation of e using Talor Series Expansion\n";
+
+  //This is the for loop that will be used to automate the taylor series expansion
+  for(int i = 0; i < valIterations; i++){
+    taylorExpansion(i);
+  } 
+
+  cout << "\n";
+
+  //End of functiom
+  return;
+
+}
+
 int main(){
   //Calling limitApproximation function
   limitApproximation();
 
-    return 0;
+  //using the taylorExpansion function
+  int uptoVal = 10;
+  automateTaylorSeries(uptoVal);
+
+
+  //End of function
+  return 0;
 }
