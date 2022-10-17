ab0037a0087a826832c54d995bfc162c183e172d
Abdon02 <averdejo02@utexas.edu>
2022-10-17 15:43:30 -0500

    Made it into a function

--- hw5/e_approximation.cpp
+++ hw5/e_approximation.cpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
-int main(){
-    double n;
-    double e_approx;
-    
-    for(int i = 1; i <= 10; i++){
-        n = pow(10,i);
-        e_approx = pow((1 + (1/n)), n);
+void limitApproximation(){
+  double n;
+  double e_approx;
+  
+  //Starting remark to the user
+  cout << "This is approximation of e using the limit method\n";
 
-        cout << "The approximation of e with n = 10^" << i << " is "  << e_approx << endl;
-    }
+  for(int i = 1; i <= 10; i++){
+    n = pow(10, i);
+    e_approx = pow((1 + (1 / n)), n);
+    
+    //This is print statement
+    cout << "The approximation of e with n = 10^" << i << " is " << e_approx << endl;
+  }
+
+  //End of function
+  return;
+}
+
+int main(){
+  //Calling limitApproximation function
+  limitApproximation();
 
     return 0;
 }
